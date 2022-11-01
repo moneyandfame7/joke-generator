4ac2075736f5988d6b86cad4b4f97f1ad3677e06
moneyandfame7 <davidoo1234e@gmail.com>
2022-11-01 09:43:27 +0200

    feat: card loader and tooltip was added

--- src/components/CardLoader.tsx
+++ src/components/CardLoader.tsx
@@ -2,13 +2,13 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
 
-const CardLoader=() =>{
+const CardLoader = () => {
     return (
-            <Box sx={{ width: 300 }}>
-                <Skeleton width={150} />
-                <Skeleton animation="wave" />
-                <Skeleton animation={false} width={150} />
-            </Box>
-            );
+        <Box sx={{width: 300}}>
+            <Skeleton  width={150} sx={{bgcolor: '#DDC3A5', opacity: 0.3}}/>
+            <Skeleton  sx={{bgcolor: '#DDC3A5', opacity: 0.3}}/>
+            <Skeleton  width={150} sx={{bgcolor: '#DDC3A5', opacity: 0.3}}/>
+        </Box>
+    );
 }
 export default CardLoader
